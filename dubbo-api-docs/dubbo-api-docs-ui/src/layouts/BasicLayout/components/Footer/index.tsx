39175b43de08a0f50c551459435e1348f53637d3
qq213539 <213539@qq.com>
2020-11-16 09:37:18 +0800

    修改前端页脚内容

--- dubbo-api-docs/dubbo-api-docs-ui/src/layouts/BasicLayout/components/Footer/index.tsx
+++ dubbo-api-docs/dubbo-api-docs-ui/src/layouts/BasicLayout/components/Footer/index.tsx
@@ -20,9 +20,7 @@ import styles from './index.module.scss';
 export default function Footer() {
   return (
     <p className={styles.footer}>
-      <span className={styles.logo}>Dubbo Doc</span>
-      <br />
-      <span className={styles.copyright}>Mail: 213539@qq.com</span>
+      <span className={styles.logo}>Dubbo API Docs</span>
     </p>
   );
 }
