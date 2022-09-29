e8c07017fa62e05bb2396146db0d4b88d816b3cc
laohu <2732554140@qq.com>
2022-09-29 17:30:14 +0800

    fix(*):1. 修护异常

--- dubbo-registry-extensions/dubbo-registry-nameservice/src/main/java/org/apache/dubbo/registry/nameservice/NameServiceRegistry.java
+++ dubbo-registry-extensions/dubbo-registry-nameservice/src/main/java/org/apache/dubbo/registry/nameservice/NameServiceRegistry.java
@@ -43,7 +43,6 @@ import org.apache.rocketmq.tools.admin.MQAdminExt;
 
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
