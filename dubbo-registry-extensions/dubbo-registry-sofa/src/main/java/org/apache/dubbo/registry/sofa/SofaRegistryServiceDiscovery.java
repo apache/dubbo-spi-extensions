10b68fb6c7e27385430b0746eadbe5478653511f
songxiaosheng <songxiaosheng@elastic.link>
2024-01-18 19:24:33 +0800

    :arrow_up:  opt dubbo version to 3.2.9

--- dubbo-registry-extensions/dubbo-registry-sofa/src/main/java/org/apache/dubbo/registry/sofa/SofaRegistryServiceDiscovery.java
+++ dubbo-registry-extensions/dubbo-registry-sofa/src/main/java/org/apache/dubbo/registry/sofa/SofaRegistryServiceDiscovery.java
@@ -16,7 +16,7 @@
  */
 package org.apache.dubbo.registry.sofa;
 
-import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson2.JSON;
 import org.apache.dubbo.common.URL;
 import org.apache.dubbo.common.logger.Logger;
 import org.apache.dubbo.common.logger.LoggerFactory;
