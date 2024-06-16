de4174a8c3642b34b3ade449ae4f32eac83df936
王聪洋 <56506697+wcy666103@users.noreply.github.com>
2024-06-17 07:52:31 +0800

    move jdk-serialize and add test cases (#417)
    * move jdk-serialize and add test cases
    
    * rename package
    
    * repalce old license header
    
    * restore Redundant renaming

--- test/scenarios/scenarios-dubbo-serialization/dubbo-serialization-jdk-test/src/main/java/org/apache/dubbo/test/serialization/jdk/EmbeddedZooKeeper.java
+++ test/scenarios/scenarios-dubbo-serialization/dubbo-serialization-jdk-test/src/main/java/org/apache/dubbo/test/serialization/jdk/EmbeddedZooKeeper.java
@@ -17,7 +17,7 @@
  *
  */
 
-package org.apache.dubbo.test.serialization.protostuff;
+package org.apache.dubbo.test.serialization.jdk;
 
 import org.apache.zookeeper.server.ServerConfig;
 import org.apache.zookeeper.server.ZooKeeperServerMain;
